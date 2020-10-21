1c4955d5f320341d2e2ad63b7be1757d2c7e009e
pazworld <paz@moudamepo.net>
2020-10-21 21:26:29 +0900

    Red: Add test for all ones game

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -4,3 +4,7 @@
 gutter_game_test() ->
     Rolls = lists:duplicate(20, 0),
     ?assertEqual(0, bowling_game:score(Rolls)).
+
+all_ones_test() ->
+    Rolls = lists:duplicate(20, 1),
+    ?assertEqual(20, bowling_game:score(Rolls)).
