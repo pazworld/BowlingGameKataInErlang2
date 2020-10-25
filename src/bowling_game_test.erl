5ca63a8814567e8f15eacdbfd58d7124f08f4308
pazworld <paz@moudamepo.net>
2020-10-25 21:02:08 +0900

    Red: Add test for a game with spares

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -8,3 +8,7 @@ gutter_game_test() ->
 all_ones_test() ->
     Rolls = lists:duplicate(20, 1),
     ?assertEqual(20, bowling_game:score(Rolls)).
+
+one_spare_test() ->
+    Rolls = [5, 5, 3] ++ lists:duplicate(17, 0),
+    ?assertEqual(16, bowling_game:score(Rolls)).
