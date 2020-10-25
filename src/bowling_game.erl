a8a623120525bf7fa30a9fbf705a2448ca8202d9
pazworld <paz@moudamepo.net>
2020-10-25 21:13:16 +0900

    Green: It scores a game with spares

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -1,6 +1,12 @@
 -module(bowling_game).
 -export([score/1]).
 
+% All rolls are processed
 score([]) -> 0;
 
+% Spare
+score([Roll1, Roll2, NextRoll1 | Rest]) when Roll1 + Roll2 =:= 10 ->
+    10 + NextRoll1 + score([NextRoll1 | Rest]);
+
+% Not Spare
 score([Roll1, Roll2 | Rest]) -> Roll1 + Roll2 + score(Rest).
